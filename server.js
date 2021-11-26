103538bfbff7e2df1134536b833cdade593b5ae3
sungyuna1003 <sungyuna1003@gmail.com>
2021-11-26 16:51:15 +0900

    DB add(test)

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ const app = express();
 const port = 3030
 app.use(express.urlencoded({ extended: false }));
 
-
+var db;
 
 var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb+srv://admin:1234@admin.v8pd6.mongodb.net/centeradmin?retryWrites=true&w=majority', function (error, client) {
+MongoClient.connect('mongodb+srv://admin:1234@admin.v8pd6.mongodb.net/centeradmin?retryWrites=true&w=majority',
+    function (error, client) {
+    if(error)return console.log(error)
+    
+    db = client.db('centeradmin');
+
+    db.collection('customer').insertOne(
+        { _id : 1 , name: 'Yuna', center: 'Neuperlach', payment: 'cash', startdate: '2021 - 10 - 02', enddate: '2021 - 11 - 02' },
+        function (error, result) {
+        console.log('clear!')
+    });
+
     app.listen(port, function () {
         console.log('server is running!');
     });
@@ -19,9 +30,10 @@ MongoClient.connect('mongodb+srv://admin:1234@admin.v8pd6.mongodb.net/centeradmi
 app.use(express.static('public'))
 app.use('/css', express(__dirname + 'public/css'))
 
-// app.listen(port, function () {
-//     console.log('server is running');
-// });
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/login.html')
+});
+
 
 app.get('/list', function (req, res) {
     res.sendFile(__dirname + '/views/list.html')
@@ -35,3 +47,15 @@ app.get('/customer', function (req, res) {
 app.get('/login', function (req, res) {
     res.sendFile(__dirname + '/views/login.html')
 });
+
+
+app.post('/add', function (req, res) {
+    res.send('sent!~');
+    console.log(req.body.name);
+    console.log(req.body.center);
+    db.collection('customer').inserOne(
+        { name: req.body.title, center: req.body.center, payment: req.body.payment, startdate: req.body.startdate, enddate: req.body.enddate},
+        function (req, res) {
+        console.log('saved!');
+    })
+})
