37264a4515a131a31a9ee6ee924127a72f381c78
sungyuna1003 <sungyuna1003@gmail.com>
2021-11-26 15:07:57 +0900

    library- bodyparser, MongoDB installed

--- server.js
+++ server.js
@@ -2,14 +2,26 @@
 const express = require('express');
 const app = express();
 const port = 3030
+app.use(express.urlencoded({ extended: false }));
+
+
+
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://admin:1234@admin.v8pd6.mongodb.net/centeradmin?retryWrites=true&w=majority', function (error, client) {
+    app.listen(port, function () {
+        console.log('server is running!');
+    });
+    
+})
+
 
 // Static files
 app.use(express.static('public'))
 app.use('/css', express(__dirname + 'public/css'))
 
-app.listen(port, function () {
-    console.log('server is running');
-});
+// app.listen(port, function () {
+//     console.log('server is running');
+// });
 
 app.get('/list', function (req, res) {
     res.sendFile(__dirname + '/views/list.html')
